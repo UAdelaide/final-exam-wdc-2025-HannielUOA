0a5cb20e605200c84972723d70627c4e597ac5f6
HannielUOA <hanniel.almonidovar@yahoo.com>
2025-06-20 07:46:48 +0000

    [automatic commit] 2025-06-20 07:46:48 UTC ;; communications error to 208.67.222.222#53: timed out

--- part1/app.js
+++ part1/app.js
@@ -85,7 +85,7 @@ VALUES
 // Route to return books as JSON
 app.get('/', async (req, res) => {
   try {
-    const [books] = await db.execute('SELECT * FROM books');
+    const [dogs] = await db.execute('SELECT * FROM Dogs');
     res.json(books);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch books' });
