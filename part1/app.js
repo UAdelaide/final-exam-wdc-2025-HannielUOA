7eb989684dfec6003b3bbe38d4edf137be150cf7
HannielUOA <hanniel.almonidovar@yahoo.com>
2025-06-20 07:44:57 +0000

    [automatic commit] 2025-06-20 07:44:57 UTC

--- part1/app.js
+++ part1/app.js
@@ -57,13 +57,9 @@ VALUES
 VALUES
 
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-
 ('cynthia', 'cynthia@example.com', 'hashed333', 'owner'),
-
 ('hanniel', 'hanniel@example.com', 'hashed321', 'owner');
       `);
     }
