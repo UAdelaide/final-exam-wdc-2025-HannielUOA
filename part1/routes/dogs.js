0417413379647990b50a4c6b6fa43f5b2ac7fde2
HannielUOA <hanniel.almonidovar@yahoo.com>
2025-06-20 08:11:25 +0000

    [automatic commit] 2025-06-20 08:11:25 UTC

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/dogs', function(req, res, next) {
+router.get('/dogs', function async(req, res, next) {
     try {
         const [dogs] = await db.execute('SELECT * FROM Dogs');
         return res.json(dogs);
